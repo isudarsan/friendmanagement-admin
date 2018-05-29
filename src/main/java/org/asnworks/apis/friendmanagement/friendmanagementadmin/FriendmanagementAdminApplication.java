91a01f38d7da810334f8e479708603499f615062
Sudarshan <sudarsan.ambatipudi@capgemini.com>
2018-05-29 12:49:09 +0530

    Initial push for spring admin

---
+++ src/main/java/org/asnworks/apis/friendmanagement/friendmanagementadmin/FriendmanagementAdminApplication.java
@@ -0,0 +1,18 @@
+package org.asnworks.apis.friendmanagement.friendmanagementadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class FriendmanagementAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendmanagementAdminApplication.class, args);
+    }
+
+}
